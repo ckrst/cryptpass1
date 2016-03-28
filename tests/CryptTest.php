1de37631b9038ce7f9fc8852c8b2f6c5e0a2520b
vinik <vinicius.kirst@gmail.com>
2016-03-28 19:37:46 -0300

    testing

--- tests/CryptTest.php
+++ tests/CryptTest.php
@@ -1,9 +1,15 @@
 <?php
-require_once '../CryptPass.php';
-require_once '../vendor/PHPUnit/autoload.php';
+require_once 'CryptPass.php';
+require_once 'vendor/autoload.php';
 
-class CryptTest extends TestCase {
+class CryptTest extends PHPUnit_Framework_TestCase {
     function testFoo() {
-        $this->assert(1,1);
+      $cryptPass = new CryptPass();
+
+      $password = "123456";
+      $ourNumber = '123456789';
+      $hashed = '337498b31c94d5bc5c1b68873923fe14';
+
+      $this->assertEquals($hashed, $cryptPass->hashPassword($password, $ourNumber));
     }
 }
